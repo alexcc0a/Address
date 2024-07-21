b06f33a01340f555fd2828ef46cacb5995e37706
Alexander Nesterov <alex18nest@icloud.com>
2024-07-21 17:54:53 -0300

    Initial Commit

---
+++ Addresses/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Addresses
+//
+//  Created by Александр Нестеров on 21/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
